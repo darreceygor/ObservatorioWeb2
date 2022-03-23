1da08ff9f244e7cb851330e4d211594f73de610c
Diego Arreceygor <darreceygor@gmail.com>
2022-03-23 16:41:32 -0300

    ampliacion de pagina: json, js y html

--- js/main.js
+++ js/main.js
@@ -1,25 +1,13 @@
-window.onload = traerDatos();
+window.onload = datos();
 
-async function traerDatos(){
+async function datos(){
     fetch('json/fichas.json') /*toma la direccion del arhivo json a tratar*/
     .then(res => res.json()) /*traemos el archivo y lo transformamos a objeto json*/
     .then(datos =>{   /*traemos los datos para procesar*/
         let carta = document.getElementById("ficha");
 
-        for (let i in datos) {         /*pongo el valor de cada campo*/
-            
-/*             carta.innerHTML +=`
-                    <article class="item">
-                        <div class="card">
-                            <div class="iconCard">
-                                <a href="resources/categories.html"><img src="${datos[i].image}" ></a>
-                            </div>
-                            <div class="cardDetail">
-                                <h3>${datos[i].category}</h3>
-                            </div>    
-                        </div>
-                    </article>
-                    ` */
+        for (let i in datos) {
+
             carta.innerHTML +=`
                 <div class="ficha" >
                     <div class="carta">
@@ -28,7 +16,7 @@ async function traerDatos(){
                     </div>
                     <h3>${datos[i].category}</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vero corporis incidunt saepe qui commodi quasi neque veniam quam, aspernatur est beatae maxime animi sed reiciendis mollitia ducimus veritatis repellendus?</p>
-                    <button>+ Info</button>
+                    <button><a href ="${datos[i].resource}">+ Info</a></button>
                 </div>
                 `
 
