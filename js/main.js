d6ce99f091dcf7ee45fb2c355da9064f0cb9ee9f
Diego Arreceygor <darreceygor@gmail.com>
2022-03-27 02:42:35 -0300

    parital render

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ async function datos(){
         let carta = document.getElementById("ficha");
 
         for (let i in datos) {
-
             carta.innerHTML +=`
                 <div class="ficha" >
                     <div class="carta">
@@ -16,12 +15,37 @@ async function datos(){
                     </div>
                     <h3>${datos[i].category}</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vero corporis incidunt saepe qui commodi quasi neque veniam quam, aspernatur est beatae maxime animi sed reiciendis mollitia ducimus veritatis repellendus?</p>
-                    <button><a href ="${datos[i].resource}">+ Info</a></button>
+                    <button type="click" id="tipo" name="${datos[i].category}">+ Info</button>
+                    
                 </div>
                 `
-
-
-
         }  
     })
 }
+
+
+document.addEventListener("click", async e => {
+
+    let data=document.getElementById("investigacion");
+    let tipo=document.getElementById("tipo");
+
+    console.log(data)
+    console.log(tipo)
+
+    await fetch ('json/info.json')
+    .then (res => res.json())
+    .then (datos => {
+        for (let i in datos){
+            if (tipo === datos[i].category){
+                console.log(datos[i].category)
+                data.innerHTML+=`
+                <div>
+                <h1>${datos[i].name}</h1><br>
+                <h2>${datos[i].instagram}</h2><br>
+                <h2>${datos[i].facebook}</h2>
+                </div>
+                `
+            }
+        }
+    })
+})
